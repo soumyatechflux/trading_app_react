2dda3b05d0025fc56834a8fe95be2a2a7125eefb
Soumya Tiwari <soumyatiwari7247@gmail.com>
2024-06-24 11:48:12 +0530

    animation added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // App.js
 import React from "react";
-import'./App.css'
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Fade from 'react-reveal/Fade';
 
 import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
@@ -15,43 +16,44 @@ import Testimonials from "./components/Testimonials/Testimonials";
 import PricingFooter from "./components/PricingFooter/PricingFooter";
 import TermsAndCondition from "./components/TermsAndCondition/TermsAndCondition";
 import ContactUs from "./components/ContactUs/ContactUs";
- import AboutUs from "./components/AboutUs/AboutUs";
- import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+import AboutUs from "./components/AboutUs/AboutUs";
+import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+
 function App() {
   return (
-    
     <Router>
       <div className="App">
-      
         <Navbar />
         <Routes>
           <Route
             path="/"
             element={
               <>
-
+                <Fade top>
+                  <About />
+         
+                  <Service />
+            
+                  <AdditionFeature />
+            
+                  <Pricing />
+           
+                  <Howitswork />
+             
+                  <Testimonials />
               
-                <About />
-                <Service />
-                <AdditionFeature />
-                <Pricing />
-                <Howitswork />
-                <Testimonials />
-                <FAQ />
-
-
-
+             
+                  <FAQ />
+                </Fade>
               </>
             }
           />
-          <Route path="/faqs" element={<FAQ />} />          
-          <Route path="/privacy_policy" element={<PrivacyPolicy />} />
-          <Route path="/about_us" element={<AboutUs />} />
-          <Route path="/contact_us" element={<ContactUs />} />
-          <Route path="/terms_and_conditions" element={<TermsAndCondition />} />
-          <Route path="/pricing_ranges" element={<PricingFooter />} />
-
-
+          <Route path="/faqs" element={<Fade><FAQ /></Fade>} />
+          <Route path="/privacy_policy" element={<Fade><PrivacyPolicy /></Fade>} />
+          <Route path="/about_us" element={<Fade><AboutUs /></Fade>} />
+          <Route path="/contact_us" element={<Fade><ContactUs /></Fade>} />
+          <Route path="/terms_and_conditions" element={<Fade><TermsAndCondition /></Fade>} />
+          <Route path="/pricing_ranges" element={<Fade><PricingFooter /></Fade>} />
         </Routes>
         <Footer />
       </div>
