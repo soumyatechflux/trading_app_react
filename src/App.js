f2e24c71580e78f6ba4fffe8b268c87102d00344
Soumya Tiwari <soumyatiwari7247@gmail.com>
2024-06-24 12:04:35 +0530

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Fade from 'react-reveal/Fade';
+import { Fade } from 'react-awesome-reveal';
 
 import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
@@ -18,12 +18,12 @@ import TermsAndCondition from "./components/TermsAndCondition/TermsAndCondition"
 import ContactUs from "./components/ContactUs/ContactUs";
 import AboutUs from "./components/AboutUs/AboutUs";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
-import ScrollToTop from "./ScrollToTop"; 
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop />  
+      <ScrollToTop />
       <div className="App">
         <Navbar />
         <Routes>
@@ -31,7 +31,7 @@ function App() {
             path="/"
             element={
               <>
-                <Fade top>
+                <Fade direction="top">
                   <About />
                   <Service />
                   <AdditionFeature />
@@ -43,16 +43,12 @@ function App() {
               </>
             }
           />
-
-
-          <Route path="/faqs" element={<Fade top><FAQ /></Fade>} />
-          <Route path="/privacy_policy" element={<Fade top><PrivacyPolicy /></Fade>} />
-          <Route path="/about_us" element={<Fade top><AboutUs /></Fade>} />
-          <Route path="/contact_us" element={<Fade top><ContactUs /></Fade>} />
-          <Route path="/terms_and_conditions" element={<Fade top><TermsAndCondition /></Fade>} />
-          <Route path="/pricing_ranges" element={<Fade top><PricingFooter /></Fade>} />
-
-
+          <Route path="/faqs" element={<Fade direction="top"><FAQ /></Fade>} />
+          <Route path="/privacy_policy" element={<Fade direction="top"><PrivacyPolicy /></Fade>} />
+          <Route path="/about_us" element={<Fade direction="top"><AboutUs /></Fade>} />
+          <Route path="/contact_us" element={<Fade direction="top"><ContactUs /></Fade>} />
+          <Route path="/terms_and_conditions" element={<Fade direction="top"><TermsAndCondition /></Fade>} />
+          <Route path="/pricing_ranges" element={<Fade direction="top"><PricingFooter /></Fade>} />
         </Routes>
         <Footer />
       </div>
